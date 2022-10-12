4ba49ed176e6608ab838732ca5a5f8e9467eab2e
Rohit Vishwakarma <41201199+rohit-vishwakarma@users.noreply.github.com>
2022-10-12 15:47:00 +0530

    Added foreign key constraint to article (#50)

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_10_094910) do
+ActiveRecord::Schema.define(version: 2022_10_12_093005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,4 +34,5 @@ ActiveRecord::Schema.define(version: 2022_10_10_094910) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "articles", "categories"
 end
