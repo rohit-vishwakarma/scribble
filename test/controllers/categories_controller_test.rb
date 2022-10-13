628a8c41e597572ee5c304dc61317449a3d35f2c
Rohit Vishwakarma <41201199+rohit-vishwakarma@users.noreply.github.com>
2022-10-14 03:04:34 +0530

    Added tests for category controller (#67)

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,4 +6,35 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @category = create(:category)
+  end
+
+  def test_should_get_successfully_from_categories_url
+    get categories_path
+    assert_response :success
+  end
+
+  def test_should_create_category
+    post categories_path, params: { category: { name: "Getting Started" } }
+    assert_response :success
+  end
+
+  def test_shouldnt_create_category_with_existing_name
+    test_category = @category
+
+    post categories_path, params: { category: { name: test_category.name } }
+    assert_response :unprocessable_entity
+
+    response_json = response.parsed_body
+    assert_equal response_json["error"], "Name has already been taken"
+  end
+
+  def test_shouldnt_create_category_without_name
+    post categories_path, params: { category: { name: "" } }
+    assert_response :unprocessable_entity
+
+    response_json = response.parsed_body
+    assert_equal response_json["error"], "Name can't be blank"
+  end
 end
