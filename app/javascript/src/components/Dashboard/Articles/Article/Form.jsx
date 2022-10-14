c25a5acb08c02c4ef7fc6a33872d7a338b5f1123
Rohit Vishwakarma <41201199+rohit-vishwakarma@users.noreply.github.com>
2022-10-15 00:13:10 +0530

    Fixed Navbar Select Active (#74)

--- app/javascript/src/components/Dashboard/Articles/Article/Form.jsx
+++ app/javascript/src/components/Dashboard/Articles/Article/Form.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { Formik, Form } from "formik";
 import { Dropdown, Button, PageLoader } from "neetoui";
-import { Select, Input, Textarea } from "neetoui/formik";
+import { Select, Input, Textarea, BlockNavigation } from "neetoui/formik";
 import { useHistory } from "react-router-dom";
 
 import categoriesApi from "apis/categories";
@@ -55,72 +55,75 @@ const ArticleForm = ({ selectedArticle, handleSubmit }) => {
       onSubmit={handleSubmit}
     >
       {({ isSubmitting, setFieldValue }) => (
-        <Form className="mx-auto mt-8 w-6/12">
-          <div className="my-5 flex gap-x-4">
-            <Input
-              required
-              className="mr-3 w-5/12"
-              label="Article Title"
-              name="title"
-              placeholder="Enter Title"
-            />
-            <Select
-              isSearchable
-              required
-              className="w-56"
-              label="Select Category"
-              name="category_id"
-              options={CATEGORY_OPTIONS}
-              placeholder="Select Category"
-            />
-          </div>
-          <Textarea
-            required
-            label="Article Body"
-            name="body"
-            placeholder="Enter text"
-            rows={30}
-          />
-          <div className="mt-4 flex gap-2">
-            <div className="flex">
-              <Button
-                className="mr-px"
-                disabled={isSubmitting}
-                label={status}
-                loading={isSubmitting}
-                name="status"
-                size="medium"
-                style="primary"
-                type="submit"
-                onClick={() => setSubmitted(true)}
+        <>
+          <BlockNavigation />
+          <Form className="mx-auto mt-8 w-6/12">
+            <div className="my-5 flex gap-x-4">
+              <Input
+                required
+                className="mr-3 w-5/12"
+                label="Article Title"
+                name="title"
+                placeholder="Enter Title"
               />
-              <Dropdown>
-                <Menu>
-                  {statusListItems.map((item, idx) => (
-                    <MenuItem.Button
-                      key={idx}
-                      onClick={() => {
-                        setFieldValue(
-                          "status",
-                          item !== "Save Draft" ? "Published" : "Draft"
-                        );
-                        setStatus(item);
-                      }}
-                    >
-                      {item}
-                    </MenuItem.Button>
-                  ))}
-                </Menu>
-              </Dropdown>
+              <Select
+                isSearchable
+                required
+                className="w-56"
+                label="Select Category"
+                name="category_id"
+                options={CATEGORY_OPTIONS}
+                placeholder="Select Category"
+              />
             </div>
-            <Button
-              label="Cancel"
-              style="text"
-              type="reset"
-              onClick={() => history.push("/")}
+            <Textarea
+              required
+              label="Article Body"
+              name="body"
+              placeholder="Enter text"
+              rows={30}
             />
-          </div>
-        </Form>
+            <div className="mt-4 flex gap-2">
+              <div className="flex">
+                <Button
+                  className="mr-px"
+                  disabled={isSubmitting}
+                  label={status}
+                  loading={isSubmitting}
+                  name="status"
+                  size="medium"
+                  style="primary"
+                  type="submit"
+                  onClick={() => setSubmitted(true)}
+                />
+                <Dropdown>
+                  <Menu>
+                    {statusListItems.map((item, idx) => (
+                      <MenuItem.Button
+                        key={idx}
+                        onClick={() => {
+                          setFieldValue(
+                            "status",
+                            item !== "Save Draft" ? "Published" : "Draft"
+                          );
+                          setStatus(item);
+                        }}
+                      >
+                        {item}
+                      </MenuItem.Button>
+                    ))}
+                  </Menu>
+                </Dropdown>
+              </div>
+              <Button
+                label="Cancel"
+                style="text"
+                type="reset"
+                onClick={() => history.push("/")}
+              />
+            </div>
+          </Form>
+        </>
       )}
     </Formik>
   );
--- app/javascript/src/components/routeConstants.js
+++ app/javascript/src/components/routeConstants.js
@@ -3,8 +3,8 @@ import Create from "./Dashboard/Articles/Article/Create";
 import Edit from "./Dashboard/Articles/Article/Edit";
 
 export const DASHBOARD_PATH = "/";
-export const SETTINGS_PATH = "/settings/general";
-export const ARTICLE_CREATE_PATH = "/article/crate";
+export const SETTINGS_PATH = "/settings";
+export const ARTICLE_CREATE_PATH = "/article/create";
 export const ARTICLE_EDIT_PATH = "/article/:slug/edit";
 
 export const DASHBOARD_ROUTES = [
