fcaf3e985770bdae9869a885ab68d7870f30d38f
Rohit Vishwakarma <41201199+rohit-vishwakarma@users.noreply.github.com>
2022-10-16 22:57:27 +0530

    Fixed redirections according to Redirections List (#93)

--- app/javascript/src/components/Dashboard/Settings/Redirections/Redirection/Form.jsx
+++ app/javascript/src/components/Dashboard/Settings/Redirections/Redirection/Form.jsx
@@ -10,6 +10,12 @@ import redirectionsApi from "apis/redirections";
 const Form = ({ setShowRedirection, initialValues, isEdit, refetch }) => {
   const handleSubmit = async values => {
     try {
+      if (values.from === values.to) {
+        Toastr.warning("From path and To path must not be same.");
+
+        return;
+      }
+
       if (isEdit) {
         await redirectionsApi.update(initialValues.id, {
           from: values.from,
@@ -31,8 +37,8 @@ const Form = ({ setShowRedirection, initialValues, isEdit, refetch }) => {
     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
       <div className="my-2 w-full">
         <FormikForm className="flex">
-          <Input required className="mx-2 w-2/5" name="from" type="url" />
-          <Input required className="w-2/5" name="to" type="url" />
+          <Input required className="mx-2 w-2/5" name="from" type="text" />
+          <Input required className="w-2/5" name="to" type="text" />
           <Button
             icon={Close}
             size={13}
