3a83dfeefd1c09d51dede41563ddf6a224aed2d1
Rohit Vishwakarma <rohit06102000@gmail.com>
2022-10-18 16:19:10 +0530

    Added No article found in EUI

---
+++ app/javascript/src/components/EUI/utils.js
@@ -0,0 +1,37 @@
+export const setIndexOfSelectedCategory = (categories, setSelectedCategory) => {
+  const currentPathSplitBySlash = window.location.pathname.split("/");
+  const lastIndex = currentPathSplitBySlash.length - 1;
+  const currentSlug = currentPathSplitBySlash[lastIndex];
+  let isSlugFound = false;
+
+  if (currentSlug !== "") {
+    categories.forEach((category, idx) =>
+      category.publishedArticles.filter(article => {
+        const isSlugMatched = article.slug === currentSlug;
+        if (isSlugMatched) {
+          isSlugFound = true;
+          setSelectedCategory(idx);
+        }
+
+        return isSlugMatched;
+      })
+    );
+  }
+
+  if (!isSlugFound && window.location.pathname !== "/public/") {
+    setSelectedCategory(-1);
+  }
+};
+
+export const findDefaultPreviewPath = (
+  categories,
+  setDefaultPreviewPath,
+  setSelectedCategory
+) => {
+  const defaultCategory = categories.find(
+    category => category.publishedArticles.length !== 0
+  );
+
+  setDefaultPreviewPath(defaultCategory.publishedArticles[0].slug);
+  setSelectedCategory(categories.indexOf(defaultCategory));
+};
