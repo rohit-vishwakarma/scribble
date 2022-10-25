63b921a79d1bf1edfc3cbcac847895aa211e0f09
Rohit Vishwakarma <41201199+rohit-vishwakarma@users.noreply.github.com>
2022-10-26 03:05:30 +0530

    Fixed Articles table to redirect published article to EUI (#203)

--- app/javascript/src/components/Dashboard/Articles/Table/index.jsx
+++ app/javascript/src/components/Dashboard/Articles/Table/index.jsx
@@ -24,6 +24,7 @@ const Table = ({ articles, refetch, handleEdit }) => {
       </Typography>
       <div className="w-full">
         <NeetoUITable
+          allowRowClick={false}
           columnData={buildArticleTableColumnData(handleDelete, handleEdit)}
           currentPageNumber={currentPageNumber}
           defaultPageSize={10}
