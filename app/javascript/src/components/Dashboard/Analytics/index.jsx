f3cacf417428895f2314beaf48444bd062d8c56f
Rohit Vishwakarma <rohit06102000@gmail.com>
2022-10-31 18:08:32 +0530

    Made Analytics Page UI

---
+++ app/javascript/src/components/Dashboard/Analytics/index.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+
+import { Table, PageLoader } from "neetoui";
+
+import articlesApi from "apis/articles";
+
+import { ArticleColumnsData } from "./utils";
+
+const Analytics = () => {
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const {
+        data: { published_articles: publishedArticles },
+      } = await articlesApi.fetch();
+      setArticles(publishedArticles);
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen w-screen">
+        <PageLoader />
+      </div>
+    );
+  }
+
+  return (
+    <div className="mx-64 mt-8">
+      <Table
+        allowRowClick={false}
+        columnData={ArticleColumnsData}
+        currentPageNumber={currentPageNumber}
+        defaultPageSize={10}
+        handlePageChange={e => setCurrentPageNumber(e)}
+        rowData={articles}
+      />
+    </div>
+  );
+};
+
+export default Analytics;
---
+++ app/javascript/src/components/Dashboard/Analytics/utils.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import { Typography } from "neetoui";
+import { Link } from "react-router-dom";
+
+import { formatTimeStampToDate } from "components/utils";
+
+export const ArticleColumnsData = [
+  {
+    title: "Title",
+    dataIndex: "title",
+    key: "title",
+    width: "35%",
+    render: (title, { slug }) => (
+      <Link target="_blank" to={`/public/${slug}`}>
+        <Typography className="text-indigo-500" lineHeight="loose" style="h5">
+          {title}
+        </Typography>
+      </Link>
+    ),
+  },
+  {
+    title: "Date",
+    dataIndex: "updated_at",
+    key: "updated_at",
+    width: "25%",
+    render: updated_at => (
+      <Typography className="text-gray-700" style="h5">
+        {formatTimeStampToDate(updated_at)}
+      </Typography>
+    ),
+  },
+  {
+    title: "Category",
+    dataIndex: "category",
+    key: "category",
+    width: "25%",
+    render: category => (
+      <Typography className="text-gray-600" style="h5">
+        {category.name}
+      </Typography>
+    ),
+  },
+  {
+    title: "Visits",
+    dataIndex: "visits",
+    key: "visits",
+    width: "15%",
+    render: () => (
+      <Typography className="text-gray-600" style="h5">
+        0
+      </Typography>
+    ),
+  },
+];
