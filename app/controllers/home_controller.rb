0aef160a02990fb822eb5c08c61218e6ea208454
Rohit Vishwakarma <41201199+rohit-vishwakarma@users.noreply.github.com>
2022-11-01 23:33:32 +0530

    Implemented redirections with backend (#231)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  before_action :redirect, only: :index
+
   def index
     render
   end
+
+  private
+
+    def redirect
+      from_path = request.path
+      redirection = Redirection.find_by(from: from_path)
+      redirect_to redirection.to if redirection != nil
+    end
 end
