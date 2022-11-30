76f01ca890bca4214ec726a6b8b434b1f4304e06
Rohit Vishwakarma <41201199+rohit-vishwakarma@users.noreply.github.com>
2022-11-30 19:41:41 +0530

    Added alert for confirm cancellation of article scheduled status (#350)

--- app/javascript/src/components/Dashboard/Articles/Article/Form.jsx
+++ app/javascript/src/components/Dashboard/Articles/Article/Form.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { Formik, Form as FormikForm } from "formik";
 import { Info } from "neetoicons";
-import { Dropdown, Button, PageLoader, Callout } from "neetoui";
+import { Dropdown, Button, PageLoader, Callout, Alert } from "neetoui";
 import { Select, Input, Textarea } from "neetoui/formik";
 import { useHistory } from "react-router-dom";
 
@@ -25,6 +25,7 @@ const Form = ({
     selectedArticle.status === "Published" ? "Publish" : "Save draft"
   );
   const [categories, setCategories] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
 
   const history = useHistory();
   const { Menu, MenuItem } = Dropdown;
@@ -45,17 +46,41 @@ const Form = ({
     }
   };
 
-  const handleCancelScheduled = async status => {
+  const handleCancelScheduled = async scheduledStatus => {
     try {
-      if (status === "Publish later") {
+      if (
+        scheduledStatus === "Publish later" &&
+        (selectedArticle.status === "Published" ||
+          selectedArticle.scheduledUnpublish === null)
+      ) {
         await articlesApi.update(selectedArticle.id, {
           scheduled_publish: null,
         });
-      } else {
+        refetch();
+      } else if (
+        scheduledStatus === "Unpublish later" &&
+        (selectedArticle.status === "Draft" ||
+          selectedArticle.scheduledPublish === null)
+      ) {
         await articlesApi.update(selectedArticle.id, {
           scheduled_unpublish: null,
         });
+        refetch();
+      } else {
+        setShowAlert(true);
       }
+    } catch (error) {
+      logger.error(error);
+    }
+  };
+
+  const handleCancelScheduledSubmit = async () => {
+    try {
+      await articlesApi.update(selectedArticle.id, {
+        scheduled_publish: null,
+        scheduled_unpublish: null,
+      });
+      setShowAlert(false);
       refetch();
     } catch (error) {
       logger.error(error);
@@ -76,123 +101,138 @@ const Form = ({
   }
 
   return (
-    <Formik
-      initialValues={selectedArticle}
-      validateOnBlur={submitted}
-      validateOnChange={submitted}
-      validationSchema={ARTICLES_FORM_VALIDATION_SCHEMA(CATEGORY_OPTIONS)}
-      onSubmit={handleSubmit}
-    >
-      {({ isSubmitting, setFieldValue, dirty }) => (
-        <FormikForm className="mx-auto mt-8 w-6/12">
-          {selectedArticle.scheduledPublish !== null && (
-            <Callout className="my-2" icon={Info} style="info">
-              <div>
-                This article is scheduled to be published at "
-                {formatTimeStampToTimeAndDate(selectedArticle.scheduledPublish)}
-                ".&nbsp;
-                <span
-                  className="cursor-pointer text-indigo-500"
-                  onClick={() => handleCancelScheduled("Publish later")}
-                >
-                  Click here&nbsp;
-                </span>
-                to cancel publish scheduled.
-              </div>
-            </Callout>
-          )}
-          {selectedArticle.scheduledUnpublish !== null && (
-            <Callout className="my-2" icon={Info} style="warning">
-              <div>
-                This article is scheduled to be Unpublished at "
-                {formatTimeStampToTimeAndDate(
-                  selectedArticle.scheduledUnpublish
-                )}
-                ".&nbsp;
-                <span
-                  className="cursor-pointer text-indigo-500"
-                  onClick={() => handleCancelScheduled("Unpublish later")}
-                >
-                  Click here&nbsp;
-                </span>
-                to cancel unpublish scheduled.
-              </div>
-            </Callout>
-          )}
-          <div className="my-5 flex gap-x-4">
-            <Input
-              required
-              className="mr-3 w-5/12"
-              label="Article Title"
-              name="title"
-              placeholder="Enter Title"
-            />
-            <Select
-              isSearchable
-              required
-              className="w-56"
-              label="Select Category"
-              name="category"
-              options={CATEGORY_OPTIONS}
-              placeholder="Select Category"
-            />
-          </div>
-          <Textarea
-            required
-            label="Article Body"
-            name="body"
-            placeholder="Enter text"
-            rows={30}
-          />
-          <div className="mt-4 flex gap-2">
-            <div className="flex">
-              <Tooltip
-                content="Please make any change to save."
-                disabled={isSubmitting || (isEdit && !dirty)}
-                followCursor="horizontal"
-                position="bottom"
-              >
-                <Button
-                  className="mr-px"
-                  disabled={isSubmitting || (isEdit && !dirty)}
-                  label={status}
-                  loading={isSubmitting}
-                  name="status"
-                  size="medium"
-                  style="primary"
-                  type="submit"
-                  onClick={() => {
-                    setFieldValue("status", status);
-                    setSubmitted(true);
-                  }}
-                />
-              </Tooltip>
-              <Dropdown>
-                <Menu>
-                  {articleStatusList.map((status, idx) => (
-                    <MenuItem.Button
-                      key={idx}
-                      onClick={() => {
-                        setFieldValue("status", status);
-                        setStatus(status);
-                      }}
-                    >
-                      {status}
-                    </MenuItem.Button>
-                  ))}
-                </Menu>
-              </Dropdown>
+    <>
+      <Formik
+        initialValues={selectedArticle}
+        validateOnBlur={submitted}
+        validateOnChange={submitted}
+        validationSchema={ARTICLES_FORM_VALIDATION_SCHEMA(CATEGORY_OPTIONS)}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting, setFieldValue, dirty }) => (
+          <FormikForm className="mx-auto mt-8 w-6/12">
+            {selectedArticle.scheduledPublish !== null && (
+              <Callout className="my-2" icon={Info} style="info">
+                <div>
+                  This article is scheduled to be published at "
+                  {formatTimeStampToTimeAndDate(
+                    selectedArticle.scheduledPublish
+                  )}
+                  ".&nbsp;
+                  <span
+                    className="cursor-pointer text-indigo-500"
+                    onClick={() => handleCancelScheduled("Publish later")}
+                  >
+                    Click here&nbsp;
+                  </span>
+                  to cancel publish scheduled.
+                </div>
+              </Callout>
+            )}
+            {selectedArticle.scheduledUnpublish !== null && (
+              <Callout className="my-2" icon={Info} style="warning">
+                <div>
+                  This article is scheduled to be Unpublished at "
+                  {formatTimeStampToTimeAndDate(
+                    selectedArticle.scheduledUnpublish
+                  )}
+                  ".&nbsp;
+                  <span
+                    className="cursor-pointer text-indigo-500"
+                    onClick={() => handleCancelScheduled("Unpublish later")}
+                  >
+                    Click here&nbsp;
+                  </span>
+                  to cancel unpublish scheduled.
+                </div>
+              </Callout>
+            )}
+            <div className="my-5 flex gap-x-4">
+              <Input
+                required
+                className="mr-3 w-5/12"
+                label="Article Title"
+                name="title"
+                placeholder="Enter Title"
+              />
+              <Select
+                isSearchable
+                required
+                className="w-56"
+                label="Select Category"
+                name="category"
+                options={CATEGORY_OPTIONS}
+                placeholder="Select Category"
+              />
             </div>
-            <Button
-              label="Cancel"
-              style="text"
-              type="reset"
-              onClick={() => history.push("/")}
+            <Textarea
+              required
+              label="Article Body"
+              name="body"
+              placeholder="Enter text"
+              rows={30}
             />
-          </div>
-        </FormikForm>
+            <div className="mt-4 flex gap-2">
+              <div className="flex">
+                <Tooltip
+                  content="Please make any change to save."
+                  disabled={isSubmitting || (isEdit && !dirty)}
+                  followCursor="horizontal"
+                  position="bottom"
+                >
+                  <Button
+                    className="mr-px"
+                    disabled={isSubmitting || (isEdit && !dirty)}
+                    label={status}
+                    loading={isSubmitting}
+                    name="status"
+                    size="medium"
+                    style="primary"
+                    type="submit"
+                    onClick={() => {
+                      setFieldValue("status", status);
+                      setSubmitted(true);
+                    }}
+                  />
+                </Tooltip>
+                <Dropdown>
+                  <Menu>
+                    {articleStatusList.map((status, idx) => (
+                      <MenuItem.Button
+                        key={idx}
+                        onClick={() => {
+                          setFieldValue("status", status);
+                          setStatus(status);
+                        }}
+                      >
+                        {status}
+                      </MenuItem.Button>
+                    ))}
+                  </Menu>
+                </Dropdown>
+              </div>
+              <Button
+                label="Cancel"
+                style="text"
+                type="reset"
+                onClick={() => history.push("/")}
+              />
+            </div>
+          </FormikForm>
+        )}
+      </Formik>
+      {showAlert && (
+        <Alert
+          isOpen
+          title="Confirm removal of article scheduled status"
+          message="This will remove both the scheduled status as the Drafted article
+          cannot be unpublish later without publish later and the Published article
+          cannot be publish later without Unpublish later."
+          onClose={() => setShowAlert(false)}
+          onSubmit={handleCancelScheduledSubmit}
+        />
       )}
-    </Formik>
+    </>
   );
 };
 
