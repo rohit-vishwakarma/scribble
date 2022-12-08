4c69758a5c4e8337c1e26e6a581d6c03979706cb
Rohit Vishwakarma <41201199+rohit-vishwakarma@users.noreply.github.com>
2022-12-08 17:01:25 +0530

    Added download report button in the analytics page (#362)

---
+++ app/javascript/src/components/Dashboard/Analytics/DownloadReport.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DownloadReport = () => (
+  <div className="max-w-7xl mx-auto px-4 py-2 sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-3xl">
+      <h1>Download is in progress......</h1>
+    </div>
+  </div>
+);
+
+export default DownloadReport;
--- app/javascript/src/components/routeConstants.js
+++ app/javascript/src/components/routeConstants.js
@@ -1,4 +1,5 @@
 import Analytics from "./Dashboard/Analytics";
+import DownloadReport from "./Dashboard/Analytics/DownloadReport";
 import Articles from "./Dashboard/Articles";
 import Create from "./Dashboard/Articles/Article/Create";
 import Edit from "./Dashboard/Articles/Article/Edit";
@@ -7,6 +8,7 @@ export const DASHBOARD_PATH = "/";
 export const ARTICLE_CREATE_PATH = "/article/create";
 export const ARTICLE_EDIT_PATH = "/article/:id/edit";
 export const ANALYTICS_PATH = "/analytics";
+export const ANALYTICS_REPORT_PATH = "/analytics/report";
 
 export const DASHBOARD_ROUTES = [
   {
@@ -24,5 +26,9 @@ export const DASHBOARD_ROUTES = [
   {
     path: ANALYTICS_PATH,
     component: Analytics,
+  },
+  {
+    path: ANALYTICS_REPORT_PATH,
+    component: DownloadReport,
   },
 ];
